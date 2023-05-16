f7ae47059f0caff0948d7398bc194666b3e6ecf6
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 16:03:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateIndex() {
+  const numEntrevistas = parseFloat(document.getElementById("num-entrevistas").value);
+  const numPropostas = parseFloat(document.getElementById("num-propostas").value);
+  const index = numEntrevistas / numPropostas;
+  document.getElementById("result").innerHTML = `Índice de entrevistas por proposta de contratação: ${index.toFixed(2)}`;
+}
 
